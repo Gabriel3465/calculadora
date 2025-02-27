e6a31eb04a52bf526a673f52042e3eb1ac9c7d93
Gabriel3465 <gabriel2_23@alu.uabcs.mx>
2025-02-26 17:45:04 -0700

    primer push

---
+++ src/Calculadora/calculadora.java
@@ -0,0 +1,13 @@
+package Calculadora;
+
+public class calculadora {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("dcbvduycvduyh");
+		
+		System.out.print("fin");
+
+	}
+
+}
